23772dd046081fe134fdc8142ed3bcc8bd85e4b6
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-22 17:05:18 +0900

    Update riverapp.py

--- riverapp.py
+++ riverapp.py
@@ -66,8 +66,8 @@ fig.suptitle('Kinematic Wave Results', fontsize=16)
 
 
 # Plot the inflow and input values on the same axis (axs[0, 0])
-axs[0, 0].plot(t, inflow, label='Inflow')
-axs[0, 0].plot(t, inflow, 'ro', label='Input Values')
+axs[0, 0].plot(t, inflow*dt, label='Inflow')
+axs[0, 0].plot(t, inflow*dt, 'ro', label='Input Values')
 axs[0, 0].set_xlabel('Time (seconds)')
 axs[0, 0].set_ylabel('Flow rate (CMS)')
 axs[0, 0].legend()
