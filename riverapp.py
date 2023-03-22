ea8df09350153375f9f60abd97d6e137e4bef272
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-22 15:06:48 +0900

    Update riverapp.py

--- riverapp.py
+++ riverapp.py
@@ -3,46 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def kinematic_wave_equation(B, L, So, n, Q0, dx, dt, Qin):
+    # Compute number of spatial and temporal steps
+    nx = int(np.ceil(L/dx))
+    nt = len(Qin)
 
+    # Initialize arrays
+    h = np.zeros((nt, nx))  # flow depth in ft
+    Q = np.zeros((nt, nx))  # flow rate in cfs
 
-# Compute number of spatial and temporal steps
+    # Initial conditions
+    Q[0, :] = Q0
+    h[0, :] = Q[0, :]/(B*So)**0.5/n**0.6  # uniform flow
 
-nx = int(np.ceil(L/dx))
-nt = len(Qin)
+    # Compute the Courant number
+    C = dt/(dx/(B*h[0, :])**0.5)
 
-# Initialize arrays
-h = np.zeros((nt, nx))  # flow depth in ft
-Q = np.zeros((nt, nx))  # flow rate in cfs
+    # Loop over time steps
+    for i in range(1, nt):
+        # Compute the slope
+        S = np.gradient(h[i-1, :])/dx + So
 
-# Initial conditions
-Q[0, :] = Q0
-h[0, :] = Q[0, :]/(B*So)**0.5/n**0.6  # uniform flow
+        # Compute the flow rate
+        Q[i, :] = Q[i-1, :] - C*B*h[i-1, :]**2*S
 
-# Compute the Courant number
-C = dt/(dx/(B*h[0, :])**0.5)
+        # Compute the flow depth
+        h[i, :] = (Q[i, :]/(B*S**0.5*n**0.6))**(3/5)
 
-# Loop over time steps
-for i in range(1, nt):
-    # Compute the slope
-    S = np.gradient(h[i-1, :])/dx + So
+        # Apply boundary conditions
+        h[i, 0] = h[i, 1]
+        Q[i, 0] = Q[i, 1]
+        h[i, -1] = h[i, -2]
+        Q[i, -1] = Q[i, -2]
 
-# Compute the flow rate
-    Q[i, :] = Q[i-1, :] - C*B*h[i-1, :]**2*S
+    # Return results
+    return Q
 
-# Compute the flow depth
-    h[i, :] = (Q[i, :]/(B*S**0.5*n**0.6))**(3/5)
-
-# Apply boundary conditions
-    h[i, 0] = h[i, 1]
-    Q[i, 0] = Q[i, 1]
-    h[i, -1] = h[i, -2]
-    Q[i, -1] = Q[i, -2]
-
-# Return results
-return Q
-
-
-Constants and parameters
+# Constants and parameters
 B = 200.0 # channel width in ft
 L = 24000.0 # channel length in ft
 So = 0.01 # bed slope
@@ -51,7 +47,7 @@ Q0 = 2000.0 # initial flow rate in cfs
 dx = 3000.0 # spatial step size in ft
 dt = 180.0 # temporal step size in sec
 
-Inflow hydrograph
+# Inflow hydrograph
 t = np.arange(0, 7200, dt) # time vector in sec
 Qin = np.zeros_like(t) # inflow vector in cfs
 Qin[0:20] = 200.0
@@ -63,10 +59,10 @@ Qin[320:640] = 1600.0
 Qin[640:720] = 800.0
 Qin[720:900] = 400.0
 
-Run the model
+# Run the model
 Q = kinematic_wave_equation(B, L, So, n, Q0, dx, dt, Qin)
 
-Plot the results
+# Plot the results
 X = np.linspace(0, L, Q.shape[1])
 T, X = np.meshgrid(t/3600, X)
 fig, ax = plt.subplots()
@@ -75,4 +71,6 @@ ax.set_xlabel('Time (hrs.)')
 ax.set_ylabel('Distance (meter)')
 ax.set_title('Flow rate (CMS)')
 plt.colorbar()
+
+# Display the plot using Streamlit
 st.pyplot(fig)
