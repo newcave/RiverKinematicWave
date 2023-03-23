b980d73f6a2312070fdc9ab26f3420be1f1551fd
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-23 18:37:25 +0900

    Update riverapp.py

--- riverapp.py
+++ riverapp.py
@@ -78,7 +78,7 @@ L = st.slider('Channel length (meters)', min_value=1000.0, max_value=100000.0, v
 So = st.slider('Channel slope', min_value=0.001, max_value=0.1, value=0.05, step=0.01)
 n = st.slider('Manning roughness coefficient', min_value=0.01, max_value=0.05, value=0.025, step=0.001)
 Q0 = st.slider('Flow rate at upstream boundary (CMS)', min_value=1.0, max_value=500.0, value=200.0)
-dx = st.slider('Distance step size (meters)', min_value=10.0, max_value=1000.0, value=500.0)
+dx = st.slider('Distance step size (meters)', min_value=100.0, max_value=1000.0, value=200.0)
 dt = st.slider('Time step size (seconds)', min_value=60, max_value=3600, value=180)
 
 #B = 100.0
